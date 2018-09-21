8c41885a92af9859ecb32abed85444b6c80bf5e6
Lucas Sindra <lucas.sindra@sap.com>
2018-09-21 13:07:52 -0300

    Inicial servidor backend

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+const app = express()
+const port = 3002
+
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200
+}
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(bodyParser.json());
+app.use(cors(corsOptions))
+
+app.get('/', (req, res) => res.send('Server is up and running'))
+
+//Get all users
+app.route('/api/users').get((req, res) => {
+  res.send({
+    users: []
+  });
+});
+
+//Get user by name
+app.route('/api/users/:username').get((req, res) => {
+    const requestedUserName = req.params['username']
+    res.send({ username: requestedUserName });
+});
+
+//Register user
+app.route('/api/users').post((req, res) => {
+  res.send(201, req.body);
+  console.log(`Registering user!`);
+});
+
+//Update user
+app.route('/api/users/:username').put((req, res) => {
+  res.send(200, req.body);
+});
+
+//Delete user
+app.route('/api/cats/:name').delete((req, res) => {
+  res.sendStatus(204);
+});
